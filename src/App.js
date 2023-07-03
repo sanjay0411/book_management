501d06f2970d1712598b92c16cb946a2947d634c
sanjay0411 <sanjaykumarbe575@gmail.com>
2023-07-03 22:51:04 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useContext } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import BooksContext from './context/books';
+
+function App() {
+  const { fetchBooks } = useContext(BooksContext);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
+    </div>
+  );
+}
+
+export default App;
